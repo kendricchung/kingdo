001949f8103b1dadb83ef4c1307bc8108624633b
v9k1b <v9k1b@ugrad.cs.ubc.ca>
2020-07-26 18:48:05 -0700

    created top bar for home page

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import TopBar from "./TopBar";
 
 class HomePage extends Component {
     render() {
-        return (<h1>Hello World!</h1>);
+        return (
+            <div style={{background: "#d6bf89"}}>
+                <TopBar></TopBar>
+                <h1>Hello World!</h1>
+            </div>
+        );
     }
 }
 
