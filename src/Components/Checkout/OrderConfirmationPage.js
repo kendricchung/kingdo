9b7849daadd893195f48cdece99878c2902579bd
Kendric Chung <kendiv3@gmail.com>
2020-10-25 23:44:11 -0700

    updated env name

--- src/Components/Checkout/OrderConfirmationPage.js
+++ src/Components/Checkout/OrderConfirmationPage.js
@@ -20,8 +20,8 @@ import Modal from "@material-ui/core/Modal";
 import CloseIcon from "@material-ui/icons/Close";
 import IconButton from "@material-ui/core/IconButton";
 
-const hostEndpoint = process.env.KINGDO_HOST_ENDPOINT
-  ? process.env.KINGDO_HOST_ENDPOINT
+const hostEndpoint = process.env.REACT_APP_KINGDO_HOST_ENDPOINT
+  ? process.env.REACT_APP_KINGDO_HOST_ENDPOINT
   : "http://localhost:4000";
 
 class OrderConfirmationPage extends Component {
